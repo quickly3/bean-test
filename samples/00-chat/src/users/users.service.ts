0d0e5e3f828efcf5fd676db26aa7bac9a25ca39d
Hongbin Zhou <>
2024-04-02 14:53:16 +0800

    upload

---
+++ samples/00-chat/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async findOne(username): Promise<any> {
+    return this.usersRepository.findOne(username);
+  }
+
+  async createOne(user): Promise<any> {
+    const createOne = await this.usersRepository.createOne(user);
+    return createOne;
+  }
+
+  async getAll(): Promise<any> {
+    const createOne = await this.usersRepository.getAll();
+    return createOne;
+  }
+}
