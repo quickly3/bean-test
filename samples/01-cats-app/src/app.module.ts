851aa4f646974a3dbdb03e78821caabb05059cc2
Hongbingzhou <jetbean486@gmail.com>
2024-04-06 12:46:20 +0800

    no message

--- samples/01-cats-app/src/app.module.ts
+++ samples/01-cats-app/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CoreModule, CatsModule],
 })
 export class AppModule {}
